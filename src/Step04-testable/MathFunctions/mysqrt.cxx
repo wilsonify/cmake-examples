c278838f00617e98d9f472cb52fb4f865c9b1a13
wilsonify <tom.a.wilson@gmail.com>
2021-07-05 14:41:41 -0400

    handle 0 and negative

--- src/Step04-testable/MathFunctions/mysqrt.cxx
+++ src/Step04-testable/MathFunctions/mysqrt.cxx
@@ -2,28 +2,29 @@
 #include "MathFunctions.h"
 
 // a hack square root calculation using simple operations
-double mysqrt(double x)
-{
-  if (x <= 0)
-    {
-    return 0;
-    }
-
+double mysqrt(double x) {
   double result;
-  double delta;
   result = x;
-
-  // do ten iterations
-  int i;
-  for (i = 0; i < 10; ++i)
-    {
-    if (result <= 0)
-      {
-      result = 0.1;
+  if (x < 0) {
+    fprintf(stdout,"complex numbers are unsupported\n");
+    result = 0;
+  }
+  else if (x == 0) {
+    fprintf(stdout,"0 is a special case \n");
+    result = 0;
+  }
+  else {
+    double delta;
+    int i;
+    for (i = 0; i < 10; ++i) {
+      if (result <= 0) {
+        fprintf(stdout,"result must be positive");
+        result = 0.1;
+        }
+      delta = x - (result*result);
+      result = result + 0.5*delta/result;
+      fprintf(stdout,"Computing sqrt of %g to be %g\n",x,result);
       }
-    delta = x - (result*result);
-    result = result + 0.5*delta/result;
-    fprintf(stdout,"Computing sqrt of %g to be %g\n",x,result);
     }
   return result;
 }
