312d990e305ea464becc765a44de26e4354d70c6
wilsonify <tom.a.wilson@gmail.com>
2021-07-31 12:39:45 -0400

    create CTestConfig.cmake

---
+++ src/Step08-dashboard/CTestConfig.cmake
@@ -0,0 +1,7 @@
+
+set(CTEST_PROJECT_NAME "cmake-examples")
+SET(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "172.19.0.3")
+set(CTEST_DROP_LOCATION "/submit.php?project=cmake-examples")
+set(CTEST_DROP_SITE_CDASH TRUE)
