588e67748b878d03fd1986535c706fd9290b4533
wilsonify <tom.a.wilson@gmail.com>
2022-12-03 19:08:41 -0500

    docker build for Step02

--- src/Step02-versioned/tutorial.cxx
+++ src/Step02-versioned/tutorial.cxx
@@ -1,11 +1,11 @@
 //A simple program that computes the square root of a number
 #include <stdio.h>
 #include <math.h>
-#include "TutorialConfig.h"
+#include "tutorialConfig.h"
 int main (int argc, char *argv[])
 {
 if (argc < 2) {
-fprintf(stdout,"%s Version %d.%d\n", argv[0], Tutorial_VERSION_MAJOR, Tutorial_VERSION_MINOR);
+fprintf(stdout,"%s Version %d.%d\n", argv[0], tutorial_VERSION_MAJOR, tutorial_VERSION_MINOR);
 fprintf(stdout, "Usage: %s number\n", argv[0]);
 return 1;
 }
